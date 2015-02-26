5d4ef1e5b5ff6367377dde79228e83fe46a7a972
wen777 <shih777577@gmail.com>
2015-02-26 19:37:39 +0800

    fix secondary without user

--- run.sh
+++ run.sh
@@ -1,24 +1,13 @@
 #!/bin/bash
-if [ "$REPL_MASTER" == "false" ]; then
-
-	touch /.mongodb_password_set
-	sleep 1
-
-elif [ ! -f /.mongodb_password_set ]; then
-
+if [ ! -f /.mongodb_password_set ]; then
 	/set_mongodb_password.sh
 	sleep 1
-
 fi
-
 
 if [ "$AUTH" == "yes" ]; then
     export mongodb="/usr/bin/mongod --nojournal --auth --httpinterface --rest --replSet '$MONGODB_REPLICA_SET' --noprealloc --smallfiles"
-
-elif [ "$REPL_MASTER" == "false" ]; then
-		export mongodb="/usr/bin/mongod --nojournal --httpinterface --rest --replSet '$MONGODB_REPLICA_SET' --noprealloc --smallfiles"
 else
-    export mongodb='/usr/bin/mongod --nojournal --httpinterface --rest --noprealloc --smallfiles'
+    export mongodb='/usr/bin/mongod --nojournal --httpinterface --rest --replSet '$MONGODB_REPLICA_SET' --noprealloc --smallfiles'
 fi
 
 
